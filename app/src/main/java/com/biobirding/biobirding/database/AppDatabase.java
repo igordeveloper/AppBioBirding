0160c6423118b1e3c26d008fde0fd6a94c7fcf52
Igor Kusmitsch <igor.kusmitsch@gmail.com>
2018-09-24 23:56:31 -0300

    Database

--- app/src/main/java/com/biobirding/biobirding/database/AppDatabase.java
+++ app/src/main/java/com/biobirding/biobirding/database/AppDatabase.java
@@ -4,22 +4,25 @@ import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 
+import com.biobirding.biobirding.dao.LastUpdateDao;
 import com.biobirding.biobirding.dao.PopularNameDao;
-import com.biobirding.biobirding.dao.SpeciesDao;
+import com.biobirding.biobirding.entity.LastUpdate;
 import com.biobirding.biobirding.entity.PopularName;
 import com.biobirding.biobirding.entity.Species;
+import com.biobirding.biobirding.dao.SpeciesDao;
 
-@Database(entities = {Species.class, PopularName.class}, version = 1, exportSchema = false)
+@Database(entities = {Species.class, PopularName.class, LastUpdate.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase INSTANCE;
 
     public abstract SpeciesDao speciesDao();
     public abstract PopularNameDao popularNameDao();
+    public abstract LastUpdateDao lastUpdateDao();
 
     public static AppDatabase getAppDatabase(Context context) {
         if (INSTANCE == null) {
-            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "Species")
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "BioBirding")
                             .build();
         }
         return INSTANCE;
