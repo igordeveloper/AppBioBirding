9e46241c75e02cfeb368066234a5577468f5377e
Igor Kusmitsch <igor.kusmitsch@gmail.com>
2018-11-17 15:54:56 -0200

    helper

--- app/src/main/java/com/biobirding/biobirding/helper/CustomSnackBar.java
+++ app/src/main/java/com/biobirding/biobirding/helper/CustomSnackBar.java
@@ -2,6 +2,7 @@ package com.biobirding.biobirding.helper;
 
 import android.graphics.Typeface;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,7 +13,7 @@ public class CustomSnackBar{
     public static void make(View view, String message){
 
         Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG);
-        snackbar.getView().setBackgroundColor(view.getContext().getColor(R.color.colorPrimary));
+        snackbar.getView().setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.colorPrimary));
         TextView textView =  snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
         textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         textView.setAllCaps(true);
